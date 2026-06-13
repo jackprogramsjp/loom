df0d772231a37ddc5c18237852129901cd7ce122
R-unic <roleyspersonal@gmail.com>
2026-06-12 21:04:25 -0400

    test: fix file manager test case

--- Loom.Testing/FileManagerTest.cs
+++ Loom.Testing/FileManagerTest.cs
@@ -6,10 +6,11 @@ public class FileManagerTest
     [Fact]
     public void Loads_Single()
     {
-        var file = FileManager.LoadSingle($"{AssemblyFixture.TestFiles}/my-file.loom");
+        var file = FileManager.LoadSingle($"{AssemblyFixture.TestFiles}/src/my-file.loom");
         Assert.Equal("my-file.loom", file.Name);
-        Assert.Equal($"{AssemblyFixture.TestFiles}{Path.DirectorySeparatorChar}my-file.loom", file.RelativePath());
-        Assert.Equal("my-file.loom", file.RelativePath(AssemblyFixture.TestFiles));
+        Assert.Equal($"{AssemblyFixture.TestFiles}{Path.DirectorySeparatorChar}src{Path.DirectorySeparatorChar}my-file.loom", file.RelativePath());
+        Assert.Equal($"src{Path.DirectorySeparatorChar}my-file.loom", file.RelativePath(AssemblyFixture.TestFiles));
+        Assert.Equal("my-file.loom", file.RelativePath(AssemblyFixture.TestFiles + "/src"));
         Assert.Equal("1 + 2", file.SourceText);
     }
 }
