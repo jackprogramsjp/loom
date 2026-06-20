32d15f2932f6b636be83833afda9ab4a21e10792
R-unic <roleyspersonal@gmail.com>
2026-06-19 20:33:37 -0400

    fix: create directories necessary to write output files

--- Loom/FileManager.cs
+++ Loom/FileManager.cs
@@ -6,11 +6,20 @@ public static class FileManager
 {
     public const string LoomExtension = ".loom";
     
-    public static void WriteCompiledFile(CompiledFile file) => File.WriteAllText(file.Path, file.RenderedLuau);
-    public static SourceFile LoadSingle(string path) => new(Path.GetFullPath(path));
-    public static List<SourceFile> LoadDirectory(string directoryPath) => LoadDirectory(directoryPath, SearchOption.AllDirectories);
+    public static void WriteCompiledFile(CompiledFile file)
+    {
+        var directory = Path.GetDirectoryName(file.Path);
+        if (!string.IsNullOrEmpty(directory))
+            Directory.CreateDirectory(directory);
+        File.WriteAllText(file.Path, file.RenderedLuau);
+    }
 
-    public static List<SourceFile> LoadDirectory(string directoryPath, SearchOption searchOption) =>
+    public static SourceFile LoadSingle(string path) => new(Path.GetFullPath(path));
+
+    public static List<SourceFile> LoadDirectory(string directoryPath) =>
+        LoadDirectory(directoryPath, SearchOption.AllDirectories);
+
+    private static List<SourceFile> LoadDirectory(string directoryPath, SearchOption searchOption) =>
         !string.IsNullOrWhiteSpace(directoryPath) && Directory.Exists(directoryPath)
             ? Directory.GetFiles(directoryPath, $"*{LoomExtension}", searchOption).Select(LoadSingle).ToList()
             : [];
