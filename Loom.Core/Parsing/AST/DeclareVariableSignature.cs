36a22e881a5c7135acf81f790ee58dc187169bc9
R-unic <roleyspersonal@gmail.com>
2026-06-26 23:31:59 -0400

    chore: fix silly warning

--- Loom.Core/Parsing/AST/DeclareVariableSignature.cs
+++ Loom.Core/Parsing/AST/DeclareVariableSignature.cs
@@ -2,11 +2,11 @@ using Loom.Text;
 
 namespace Loom.Parsing.AST;
 
-public class DeclareVariableSignature(Token keyword, Token name, ColonTypeClause? colonTypeClause, params Node?[] extraChildren)
+public class DeclareVariableSignature(Token keyword, Token name, ColonTypeClause colonTypeClause, params Node?[] extraChildren)
     : DeclareSignature([keyword], name, [colonTypeClause, ..extraChildren])
 {
     public Token Keyword { get; } = keyword;
-    public ColonTypeClause? ColonTypeClause { get; } = colonTypeClause;
+    public ColonTypeClause ColonTypeClause { get; } = colonTypeClause;
 
     public override T Accept<T>(Visitor<T> visitor) => visitor.VisitDeclareVariableSignature(this);
 }
--- Loom.Core/Parsing/AST/VariableDeclaration.cs
+++ Loom.Core/Parsing/AST/VariableDeclaration.cs
@@ -5,7 +5,9 @@ namespace Loom.Parsing.AST;
 public class VariableDeclaration(Token keyword, Token name, ColonTypeClause? colonTypeClause, EqualsValueClause? equalsValueClause)
     : DeclareVariableSignature(keyword, name, colonTypeClause!, equalsValueClause)
 {
+    public new ColonTypeClause? ColonTypeClause { get; } = colonTypeClause;
     public EqualsValueClause? EqualsValueClause { get; } = equalsValueClause;
+
 
     public override T Accept<T>(Visitor<T> visitor) => visitor.VisitVariableDeclaration(this);
 }
