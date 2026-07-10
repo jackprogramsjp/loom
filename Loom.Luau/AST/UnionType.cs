ae6724a77956c6fc58b85a0fed06119e7372fb6c
R-unic <roleyspersonal@gmail.com>
2026-07-09 23:26:14 -0400

    fix: simplify number literal union luau emit (`69 | 420` -> `number`)

--- Loom.Luau/AST/UnionType.cs
+++ Loom.Luau/AST/UnionType.cs
@@ -2,7 +2,7 @@ namespace Loom.Luau.AST;
 
 public class UnionType(List<LuauType> types) : LuauType
 {
-    public List<LuauType> Types { get; } = types;
+    public List<LuauType> Types { get; } = types.Distinct().ToList();
 
     public override string Render(RenderState state) => string.Join(" | ", Types.ConvertAll(state.ParenthesizeIfNeeded));
 }
